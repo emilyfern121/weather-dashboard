f92bc14598c62ff959224e12b5c4bd0bbb41a943
Emily Fernandez <emilyfern121@gmail.com>
2021-08-12 21:38:31 -0400

    updated html file and js file

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,11 @@
 
 var API_KEY="9e638e156d122da7536ad49d6d5b1c35";
 
+// Display current date
+// var today = moment();
+// $("#date").text(today.format("dddd MMMM Do, YYYY"));
+
+
 document.getElementById("cityForm").addEventListener("submit",function(event) {
     event.preventDefault();
     var cityName=document.getElementById("cityNameInput").value;
@@ -9,7 +14,7 @@ document.getElementById("cityForm").addEventListener("submit",function(event) {
     } else {
         document.getElementById("cityName").innerText=cityName;
 
-        var requestUrl="http://api.openweathermap.org/geo/1.0/direct?q="+cityName+"&limit=1&appid="+API_KEY;
+        var requestUrl="http://api.openweathermap.org/geo/1.0/direct?q="+cityName+"&limit=1&appid="+API_KEY+"&units=imperial";
 
         fetch(requestUrl).then(function(response) {
             if(!response.ok) {
@@ -19,12 +24,13 @@ document.getElementById("cityForm").addEventListener("submit",function(event) {
             
         }).then(function(data) {
             console.log(data);
-
+            
             var latitude=data[0].lat;
             var longitude=data[0].lon;
 
-            var forecastRequestUrl="https://api.openweathermap.org/data/2.5/onecall?lat="+latitude+"&lon="+longitude+"&appid="+API_KEY;
-
+            var forecastRequestUrl="https://api.openweathermap.org/data/2.5/onecall?lat="+latitude+"&lon="+longitude+"&appid="+API_KEY+"&units=imperial";
+            
+            
             fetch(forecastRequestUrl).then(function(response) {
                 if(!response.ok) {
                     alert("No information found for "+cityName);
@@ -35,71 +41,18 @@ document.getElementById("cityForm").addEventListener("submit",function(event) {
 
                 document.getElementById("currentTemperature").innerText=data.current.temp;
                 document.getElementById("currentHumidity").innerText=data.current.humidity;
-                document.getElementById("currentWindSpeed").innerText=data.wind_speed;
+                document.getElementById("currentWindSpeed").innerText=data.current.wind_speed;
                 document.getElementById("currentUvi").innerText=data.current.uvi;
-
-                for(var i=0;i<data.Search.length;i++) {
-                    var dailyForecast=data.Search[i];
-                    
-                    var forecast=$("<div>");
-                    forecast.addClass("forecast");
-
-                    var dailyTemperature=$("<h1>");
-                    dailyTemperature.addClass("temperature");
-    
-                    var dailyHumidity=$("<h1>");
-                    dailyHumidity.addClass("humidity");
-
-                    forecast.append(dailyTemperature);
-    
-                    forecast.append(dailyHumidity);
-
-                    //Rest of properties
-    
-                    movieDiv.appendTo("#forecast");
-    
-    
-                }
-
+                document.getElementById("dailyTemperature").innerText=data.daily.temp;
+                document.getElementById("dailyHumidity").innerText=data.daily.humidity;
+                document.getElementById("dailyIcon").innerText=data.daily.weather.icon;
             });
+
         })
-
     }
-
 })
 
 
-// document.getElementById("cityForm").addEventListener("submit",function(event) {
-//     event.preventDefault();
-//     var cityName=document.getElementById("cityNameInput").value;
-//     if(!cityName) {
-//         alert("Please enter the name of a city.");
-//     } else {
-//         document.getElementById("cityName").innerText=cityName;
-
-//         var requestUrl="http://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+API_KEY;
-
-//         fetch(requestUrl).then(function(response) {
-//             if(!response.ok) {
-//                 alert("No information found for "+cityName);
-//             } 
-//             return response.json();
-            
-//         }).then(function(data) {
-//             console.log(data);
-
-//             document.getElementById("currentTemperature").innerText=data.main.temp;
-//             document.getElementById("currentHumidity").innerText=data.main.humidity;
-//             document.getElementById("currentWindSpeed").innerText=data.wind.speed;
-//         })
-
-//     }
-
-// })
-
-
-//  var today = moment();
-//  $("#date").text(today.format("dddd MMMM Do, YYYY"));
 
 
 
